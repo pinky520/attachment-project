26a114fc5da213e32be43f6aab6bce86cd3f7562
Michael Mhizha <mmhizha96@gmail.com>
2025-04-04 13:29:45 +0200

    aa

--- app/Http/Controllers/Admin/Auth/Venues_Controller.php
+++ app/Http/Controllers/Admin/Auth/Venues_Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Venues_;
-use App\Models\Resources_;
+use App\Models\Resources;
 
 class Venues_Controller extends Controller
 {
